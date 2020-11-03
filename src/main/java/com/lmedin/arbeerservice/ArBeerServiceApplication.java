a3a620e1ec24abef96a20e49efc9860b91e9275a
Leandro Medin <leandro.medin@valtech.com>
2020-11-03 13:02:37 -0300

    creation project

---
+++ src/main/java/com/lmedin/arbeerservice/ArBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lmedin.arbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmedin/arbeerservice/ArBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmedin.arbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
