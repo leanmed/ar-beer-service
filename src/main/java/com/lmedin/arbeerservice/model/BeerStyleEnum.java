63e371ce5845f67e1eba31f85adea2e765a9a098
Leandro Medin <leandro.medin@valtech.com>
2020-11-03 13:16:01 -0300

    add data model

---
+++ src/main/java/com/lmedin/arbeerservice/model/BeerStyleEnum.java
@@ -0,0 +1,20 @@
+package com.lmedin.arbeerservice.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum BeerStyleEnum {
+
+    LARGER("LARGER"),
+    PISLSNER("PISLSNER"),
+    ALE("ALE"),
+    STOUT("STOUT"),
+    IPA("IPA"),
+    GOLD("GOLD");
+
+    public final String typeOfBeer;
+
+    private BeerStyleEnum(String type){
+        this.typeOfBeer = type;
+    }
+}
